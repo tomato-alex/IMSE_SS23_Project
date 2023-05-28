67b76d47370055d1595e66fcdcdc29213553d196
DragomirMar <drago.mar.18@gmail.com>
2023-05-28 13:47:50 +0200

    updated sql scripts

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,17 +1,13 @@
-DROP VIEW ecars;
 DROP VIEW total_sales;
 DROP VIEW leasing_options;
-DROP TRIGGER mitarbeiter_trig;
-DROP SEQUENCE mitarbeiter_seq;
-DROP TRIGGER auto_trig;
-DROP SEQUENCE auto_seq;
 
-DROP TABLE hat;
-DROP TABLE verkauft;
-DROP TABLE mitarbeiter;
-DROP TABLE suv;
-DROP TABLE elektroauto;
-DROP TABLE auto;
+DROP TABLE has;
+DROP TABLE sells;
+DROP TABLE employee;
+DROP TABLE car;
 DROP TABLE leasing;
-DROP TABLE autowerkstatt;
-DROP TABLE filiale;
+DROP TABLE workshop;
+DROP TABLE location;
+
+DROP DATABASE imse_23_ss;
+
